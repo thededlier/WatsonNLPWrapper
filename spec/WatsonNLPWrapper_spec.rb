8cd7e9e8a8abf9e81574d88df10018de0fa545c5
Rohan Anand <thededlier@gmail.com>
2018-06-05 14:47:38 +0530

    removed default test case

--- spec/WatsonNLPWrapper_spec.rb
+++ spec/WatsonNLPWrapper_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe WatsonNLPWrapper do
   it "has a version number" do
     expect(WatsonNLPWrapper::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
