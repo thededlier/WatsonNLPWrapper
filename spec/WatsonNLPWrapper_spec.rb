a453a3d8115cd691beadee8908e4142c8d80d933
Rohan Anand <thededlier@gmail.com>
2018-06-08 15:49:51 +0530

    test for response

--- spec/WatsonNLPWrapper_spec.rb
+++ spec/WatsonNLPWrapper_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe WatsonNLPWrapper do
     @test_username = ENV['WATSON_USERNAME']
     @test_password = ENV['WATSON_PASSWORD']
     @test_version = '2017-02-27'
+    @sample_text = "I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up and live out the true meaning of its creed: \"We hold these truths to be self-evident, that all men are created equal.\""
+
+    @nlp = WatsonNLPWrapper::WatsonNLPApi.new(@test_url, @test_username, @test_password)
   end
   it "has a version number" do
     expect(WatsonNLPWrapper::VERSION).not_to be nil
@@ -23,5 +26,9 @@ RSpec.describe WatsonNLPWrapper do
     expect(nlp.instance_variable_get(:@username)).to eq @test_username
     expect(nlp.instance_variable_get(:@password)).to eq @test_password
     expect(nlp.instance_variable_get(:@version)).to eq WatsonNLPWrapper::Constants::DEFAULT_VERSION
+  end
+
+  it "should give a response" do
+    expect(@nlp.analyze(@sample_text))
   end
 end
